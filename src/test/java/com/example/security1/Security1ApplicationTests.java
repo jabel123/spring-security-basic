8a75609b3032b2b3aae27c87233ca7c4b63066ca
juhyeontae <jabel123@naver.com>
2022-04-09 22:42:23 +0900

    chap01 : 환경설정
    스프링 시큐리티 환경설정

---
+++ src/test/java/com/example/security1/Security1ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.security1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security1ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}

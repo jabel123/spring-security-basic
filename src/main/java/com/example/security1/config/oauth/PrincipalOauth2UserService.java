ae49b3c0c7771b5a2ae7a2a7aed624b64e24e46f
juhyeontae <jabel123@naver.com>
2022-04-25 18:38:34 +0900

    chap8 : Authentication객체가 가질수 있는 2가지 타입
    Authentication객체가 가질수 있는 2가지 타입

--- src/main/java/com/example/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/example/security1/config/oauth/PrincipalOauth2UserService.java
@@ -14,8 +14,11 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService
     {
         System.out.println("userRequest : " + userRequest.getClientRegistration());
         System.out.println("userRequest : " + userRequest.getAccessToken().getTokenValue());
+        // 구글 로그인 버튼 클릭 -> 구글 로그인 창 -> 로그인 완료 ->  code를 리턴(OAuth2 - Client 라이브러리) -> AccessToken 요청
+        // userRequest정보 -> loadUser함수 호출 -> 구글로부터 회원프로필 받아준다.
         System.out.println("userRequest : " + super.loadUser(userRequest).getAttributes());
 
+        OAuth2User oAuth2User = super.loadUser(userRequest);
         // 회원가입을 강제로 해볼 예정
 
         return super.loadUser(userRequest);
--- src/main/java/com/example/security1/controller/IndexController.java
+++ src/main/java/com/example/security1/controller/IndexController.java
@@ -1,11 +1,16 @@
 package com.example.security1.controller;
 
+import com.example.security1.config.auth.PrincipalDetails;
 import com.example.security1.model.User;
 import com.example.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +24,30 @@ public class IndexController
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
+    @GetMapping("/test/login")
+    public @ResponseBody String loginTest(Authentication authentication
+            , @AuthenticationPrincipal PrincipalDetails userDetails)
+    {
+        System.out.println("/test/login =========================");
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        System.out.println("authentication : " + principalDetails.getUser());
+        System.out.println("userDetails : " + userDetails.getUser());
+
+
+        return "세션정보 확인하기";
+    }
+
+    @GetMapping("/test/oauth/login")
+    public @ResponseBody String loginOauthTest(Authentication authentication,
+                                               @AuthenticationPrincipal OAuth2User oauth)
+    {
+        System.out.println("/test/oauth/login =========================");
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        System.out.println("authentication : " + oAuth2User.getAttributes());
+        System.out.println("oauth2 user : " + oauth.getAttributes());
+
+        return "세션정보 확인하기";
+    }
 
 
     // localhost:8080
